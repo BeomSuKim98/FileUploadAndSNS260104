6230cedca41ebc20a201d14353840fe78c9d6271
BeomSuKim98 <qja0427@gmail.com>
2026-01-11 20:55:44 +0900

    파일 2개 업로드 하드코딩

--- src/main/java/com/sbs/tutorial/app1/domain/fileUpload/controller/FileUploadController.java
+++ src/main/java/com/sbs/tutorial/app1/domain/fileUpload/controller/FileUploadController.java
@@ -15,9 +15,8 @@ import java.io.File;
 public class FileUploadController {
     @RequestMapping("")
     @ResponseBody
-    public String upload(@RequestParam("img1")MultipartFile file){
+    public String upload(@RequestParam("img1")MultipartFile img1, @RequestParam("img2")MultipartFile img2){
         String uploadDir = "C:/spring-temp/uploadImg";
-        String fileName = "1.png";
 
         File directory = new File(uploadDir);
 
@@ -25,11 +24,10 @@ public class FileUploadController {
             directory.mkdirs();
         }
 
-        File saveFile = new File(uploadDir, fileName);
-
         try {
-            file.transferTo(saveFile);
-            return "파일 업로드 성공 : " + saveFile.getAbsolutePath();
+            img1.transferTo(new File(directory, "1.png"));
+            img2.transferTo(new File(directory, "2.png"));
+            return "파일 업로드 성공";
         } catch (Exception e) {
             e.printStackTrace();
             return "파일 업로드 실패 : " + e.getMessage();
