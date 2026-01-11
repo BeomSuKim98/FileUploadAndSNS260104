cccb15fbb1d2de7c2739587bfd15dc14740b4286
BeomSuKim98 <qja0427@gmail.com>
2026-01-11 21:06:33 +0900

    설정파일에서 저장경로 가져오기 및 application파일수정

--- src/main/java/com/sbs/tutorial/app1/domain/fileUpload/controller/FileUploadController.java
+++ src/main/java/com/sbs/tutorial/app1/domain/fileUpload/controller/FileUploadController.java
@@ -1,6 +1,7 @@
 package com.sbs.tutorial.app1.domain.fileUpload.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,12 +14,12 @@ import java.io.File;
 @RequiredArgsConstructor
 @RequestMapping("/upload")
 public class FileUploadController {
+    @Value("${custom.genFileDirPath}")
+    private String genFileDirPath;
     @RequestMapping("")
     @ResponseBody
     public String upload(@RequestParam("img1")MultipartFile img1, @RequestParam("img2")MultipartFile img2){
-        String uploadDir = "C:/spring-temp/uploadImg";
-
-        File directory = new File(uploadDir);
+        File directory = new File(genFileDirPath);
 
         if(!directory.exists()){
             directory.mkdirs();
